c817cd24e20fa74a289507b1f49c370a03dc3d00
Isurika Arunodi <2020cs015@stu.ucsc.cmb.ac.lk>
2024-03-10 17:01:24 +0530

    feat(add): signup page added

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:talento/layout/main_layout.dart';
 import 'package:talento/themes/theme_data.dart';
 import 'package:talento/views/login_page.dart';
+import 'package:talento/views/signup_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -25,7 +26,8 @@ class MyApp extends StatelessWidget {
       theme: Themes.lightTheme,
       darkTheme: Themes.darkTheme,
       routes: {
-        '/': (context) => LoginPage(),
+        // '/': (context) => LoginPage(),
+        '/' : (context) => SignupPage(),
         '/job-applicant' : (context) => MainLayout(),
         // '/job-employer' : (context) => checkAuthentication(),
       },
