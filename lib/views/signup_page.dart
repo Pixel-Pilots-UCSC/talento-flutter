94d96a7afbe0d50b9f845b64e1fe2d3b24aee656
Dinushan <stdinushan@gmail.com>
2024-03-11 23:04:15 +0530

    Merged

--- lib/views/signup_page.dart
+++ lib/views/signup_page.dart
@@ -27,137 +27,139 @@ class SignupPage extends StatelessWidget {
     return Scaffold(
         body: SafeArea(
             child: Center(
-      child: Container(
-        //Welcome Text, Email and Password Fields, Remember Me and Forgot Password Links, Login Button
-        //with StreamBuilder for Remember Me and Password Visibility
-        alignment: Alignment.center,
-        padding: const EdgeInsets.all(16),
-        width: MediaQuery.of(context).size.width * 0.9 < 350
-            ? MediaQuery.of(context).size.width * 0.9
-            : 350,
-        child: Column(
-          children: <Widget>[
-            const Image(
-              image: AssetImage('assets/icons/logo.png'),
-              width: 150,
-              height: 150,
-            ),
-            const SizedBox(height: 4),
-            //Sign In Text
-            Container(
-              margin: const EdgeInsets.only(bottom: 16),
-              child: Column(
-                children: [
-                  const Row(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    children: [
-                      Text(
-                        'Sign Up',
-                        style: TextStyle(
-                            fontSize: 32,
-                            fontWeight: FontWeight.bold,
-                            color: Color(0xFF3EB489)),
-                      ),
-                    ],
+      child: SingleChildScrollView(
+        child: Container(
+          //Welcome Text, Email and Password Fields, Remember Me and Forgot Password Links, Login Button
+          //with StreamBuilder for Remember Me and Password Visibility
+          alignment: Alignment.center,
+          padding: const EdgeInsets.all(16),
+          width: MediaQuery.of(context).size.width * 0.9 < 350
+              ? MediaQuery.of(context).size.width * 0.9
+              : 350,
+          child: Column(
+            children: <Widget>[
+              const Image(
+                image: AssetImage('assets/icons/logo.png'),
+                width: 150,
+                height: 150,
+              ),
+              const SizedBox(height: 4),
+              //Sign In Text
+              Container(
+                margin: const EdgeInsets.only(bottom: 16),
+                child: Column(
+                  children: [
+                    const Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        Text(
+                          'Sign Up',
+                          style: TextStyle(
+                              fontSize: 32,
+                              fontWeight: FontWeight.bold,
+                              color: Color(0xFF3EB489)),
+                        ),
+                      ],
+                    ),
+                    SizedBox(height: 8),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        Text(
+                          'Unlock Your Career Potential',
+                          style: TextStyle(
+                              fontFamily: GoogleFonts.pacifico().fontFamily,
+                              fontSize: 20,
+                              fontWeight: FontWeight.normal,
+                              color: Color(0xFF61677D)),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+              const SizedBox(height: 6),
+              Container(
+                child: TextField(
+                  controller: nameController,
+                  decoration: const InputDecoration(
+                    hintText: 'Name',
                   ),
-                  SizedBox(height: 8),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    children: [
-                      Text(
-                        'Unlock Your Career Potential',
-                        style: TextStyle(
-                            fontFamily: GoogleFonts.pacifico().fontFamily,
-                            fontSize: 20,
-                            fontWeight: FontWeight.normal,
-                            color: Color(0xFF61677D)),
-                      ),
-                    ],
+                ),
+              ),
+              const SizedBox(height: 16),
+              Container(
+                child: TextField(
+                  controller: emailController,
+                  decoration: const InputDecoration(
+                    hintText: 'Email',
                   ),
+                ),
+              ),
+              const SizedBox(height: 16),
+              StreamBuilder<bool>(
+                stream: _passwordVisible.state,
+                initialData: false,
+                builder: (context, snapshot) {
+                  return TextField(
+                    controller: passwordController,
+                    obscureText: !snapshot.data!,
+                    decoration: InputDecoration(
+                      hintText: 'Password',
+                      suffixIcon: IconButton(
+                        icon: Icon(snapshot.data!
+                            ? Icons.visibility
+                            : Icons.visibility_off),
+                        onPressed: _passwordVisible.toggle,
+                      ),
+                    ),
+                  );
+                },
+              ),
+              const SizedBox(height: 16),
+              StreamBuilder<bool>(
+                stream: _confirmPasswordVisible.state,
+                initialData: false,
+                builder: (context, snapshot) {
+                  return TextField(
+                    controller: passwordController,
+                    obscureText: !snapshot.data!,
+                    decoration: InputDecoration(
+                      hintText: 'Confirm Password',
+                      suffixIcon: IconButton(
+                        icon: Icon(snapshot.data!
+                            ? Icons.visibility
+                            : Icons.visibility_off),
+                        onPressed: _passwordVisible.toggle,
+                      ),
+                    ),
+                  );
+                },
+              ),
+              const SizedBox(height: 16),
+              Container(
+                width: MediaQuery.of(context).size.width * 0.9 < 350
+                    ? MediaQuery.of(context).size.width * 0.9
+                    : 350,
+                child: ElevatedButton(
+                  onPressed: () {
+                    _signupUser();
+                  },
+                  child: const Text('Signup'),
+                ),
+              ),
+              // Don't have an account? Sign Up
+              // const SizedBox(height: 16),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: const [
+                  Text('Already have an account?'),
+                  SizedBox(width: 6),
+                  TextButton(onPressed: null, child: Text('Sign In')),
                 ],
               ),
-            ),
-            const SizedBox(height: 6),
-            Container(
-              child: TextField(
-                controller: nameController,
-                decoration: const InputDecoration(
-                  hintText: 'Name',
-                ),
-              ),
-            ),
-            const SizedBox(height: 16),
-            Container(
-              child: TextField(
-                controller: emailController,
-                decoration: const InputDecoration(
-                  hintText: 'Email',
-                ),
-              ),
-            ),
-            const SizedBox(height: 16),
-            StreamBuilder<bool>(
-              stream: _passwordVisible.state,
-              initialData: false,
-              builder: (context, snapshot) {
-                return TextField(
-                  controller: passwordController,
-                  obscureText: !snapshot.data!,
-                  decoration: InputDecoration(
-                    hintText: 'Password',
-                    suffixIcon: IconButton(
-                      icon: Icon(snapshot.data!
-                          ? Icons.visibility
-                          : Icons.visibility_off),
-                      onPressed: _passwordVisible.toggle,
-                    ),
-                  ),
-                );
-              },
-            ),
-            const SizedBox(height: 16),
-            StreamBuilder<bool>(
-              stream: _confirmPasswordVisible.state,
-              initialData: false,
-              builder: (context, snapshot) {
-                return TextField(
-                  controller: passwordController,
-                  obscureText: !snapshot.data!,
-                  decoration: InputDecoration(
-                    hintText: 'Confirm Password',
-                    suffixIcon: IconButton(
-                      icon: Icon(snapshot.data!
-                          ? Icons.visibility
-                          : Icons.visibility_off),
-                      onPressed: _passwordVisible.toggle,
-                    ),
-                  ),
-                );
-              },
-            ),
-            const SizedBox(height: 16),
-            Container(
-              width: MediaQuery.of(context).size.width * 0.9 < 350
-                  ? MediaQuery.of(context).size.width * 0.9
-                  : 350,
-              child: ElevatedButton(
-                onPressed: () {
-                  _signupUser();
-                },
-                child: const Text('Signup'),
-              ),
-            ),
-            // Don't have an account? Sign Up
-            // const SizedBox(height: 16),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: const [
-                Text('Already have an account?'),
-                SizedBox(width: 6),
-                TextButton(onPressed: null, child: Text('Sign In')),
-              ],
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     )));
